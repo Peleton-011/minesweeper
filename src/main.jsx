77e18029aa6b063358b71331b607c3d28b3c8b40
Nico Diaz <nicodiazan@gmail.com>
2024-12-22 17:21:28 +0100

    Run back some redundant style changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { enable } from "darkreader";
-
-enable({
-	brightness: 100,
-	contrast: 90,
-	sepia: 10,
-});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
