7705302ee646dd070dbed77be271f1bca7ec48ee
Nico Diaz <nicodiazan@gmail.com>
2026-04-09 20:21:59 +0200

    Update timeutils to show times in centiseconds for clarity

--- src/utils/timeutils.js
+++ src/utils/timeutils.js
@@ -1,15 +1,14 @@
-export 	const getPlayTimeString = (time) => {
-		const centis = (time % 1000).toString().padStart(2, "0");
-		const seconds = (Math.floor(time / 1000) % 60)
-			.toString()
-			.padStart(2, "0");
-		const minutes = (Math.floor(time / (1000 * 60)) % 60)
-			.toString()
-			.padStart(2, "0");
-		const hours = (Math.floor(time / (1000 * 60 * 60)) % 24)
-			.toString()
-			.padStart(2, "0");
+export const getPlayTimeString = (time) => {
+	// 
+	const centis = (time % 1000).toString().padStart(2, "0").slice(0,2);
+	const seconds = (Math.floor(time / 1000) % 60).toString().padStart(2, "0");
+	const minutes = (Math.floor(time / (1000 * 60)) % 60)
+		.toString()
+		.padStart(2, "0");
+	const hours = (Math.floor(time / (1000 * 60 * 60)) % 24)
+		.toString()
+		.padStart(2, "0");
 
-		const string = `${time > 60 * 60 * 1000 ? hours + "h " : ""}${time > 60 * 1000 ? minutes + "' " : ""}${seconds},${centis}"`;
-		return string;
-	};
+	const string = `${time > 60 * 60 * 1000 ? hours + "h " : ""}${time > 60 * 1000 ? minutes + "' " : ""}${seconds},${centis}"`;
+	return string;
+};
