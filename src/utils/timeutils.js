cd1ead482719e2d80ad1052f7b02bfdb27d0dc9d
Nico Diaz <nicodiazan@gmail.com>
2026-04-09 21:17:15 +0200

    Typo

--- src/utils/timeutils.js
+++ src/utils/timeutils.js
@@ -17,47 +17,47 @@ export const getDateString = (time) => {
 	const date = new Date(time);
 	const day = date.getDate().toString();
 	let month;
-    switch (date.getMonth()) {
-        case 0:
-            month = "Jan";
-            break;
-        case 1:
-            month = "Feb";
-            break;
-        case 2:
-            month = "Mar";
-            break;
-        case 3:
-            month = "Apr";
-            break;
-        case 4:
-            month = "May";
-            break;
-        case 5:
-            month = "Jun";
-            break;
-        case 6:
-            month = "Jul";
-            break;
-        case 7:
-            month = "Aug";
-            break;
-        case 8:
-            month = "Sep";
-            break;
-        case 9:
-            month = "Oct";
-            break;
-        case 10:
-            month = "Nov";
-            break;
-        case 11:
-            month = "Dec";
-            break;    
-        default:
-            break;
-    }
-    
+	switch (date.getMonth()) {
+		case 0:
+			month = "Jan";
+			break;
+		case 1:
+			month = "Feb";
+			break;
+		case 2:
+			month = "Mar";
+			break;
+		case 3:
+			month = "Apr";
+			break;
+		case 4:
+			month = "May";
+			break;
+		case 5:
+			month = "Jun";
+			break;
+		case 6:
+			month = "Jul";
+			break;
+		case 7:
+			month = "Aug";
+			break;
+		case 8:
+			month = "Sep";
+			break;
+		case 9:
+			month = "Oct";
+			break;
+		case 10:
+			month = "Nov";
+			break;
+		case 11:
+			month = "Dec";
+			break;
+		default:
+			break;
+	}
+
 	const year = date.getFullYear();
-	return `${day} ${month.toLowerCase}. ${year}`;
+	return `${day} ${month.toLowerCase()}. ${year}`;
 };
