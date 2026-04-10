58882e9b418cc3f779211d86c6feb0cf105d2eb3
Nico Diaz <nicodiazan@gmail.com>
2026-04-10 12:08:00 +0200

    Move over difficulties to its own util and add a difficulty namer

---
+++ src/utils/difficulties.js
@@ -0,0 +1,54 @@
+export const baseDifficulty = {
+	winStateCheck: "revealAll",
+	startZone: 3,
+	lives: 3,
+	autoSolveMode: false,
+	noGuessMode: false,
+};
+export const standardDifficulties = [
+	{
+		...baseDifficulty,
+		height: 8,
+		width: 8,
+		mineCount: 10,
+		difName: "Easy",
+	},
+	{
+		...baseDifficulty,
+		height: 16,
+		width: 16,
+		mineCount: 40,
+		difName: "Medium",
+	},
+	{
+		...baseDifficulty,
+		height: 16,
+		width: 30,
+		mineCount: 99,
+		difName: "Hard",
+	},
+];
+
+export const getDifficultyNameFromConfig = (config) => {
+	const mode = config.lives === 1 ? "Perfect" : "Arcade";
+	let difficulty = "";
+	console.log(config);
+	standardDifficulties.forEach((dif) => {
+		const size = dif.width * dif.height;
+		console.log(
+			size,
+			", ",
+			config.size,
+			" - ",
+			dif.mineCount,
+			", ",
+			config.mines,
+		);
+		if (size === config.size && dif.mineCount === config.mines) {
+			difficulty += dif.difName;
+		}
+	});
+
+	if (difficulty.length === 0) difficulty += "Custom";
+	return mode + " " + difficulty;
+};
