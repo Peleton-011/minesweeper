443c580c3fe882c7a3c4d5de6873a4f8a43cced0
Nico Diaz <nicodiazan@gmail.com>
2026-04-14 08:45:50 +0200

    Remove unnecessary logic from the LeaderBoard

--- src/components/LeaderBoard.jsx
+++ src/components/LeaderBoard.jsx
@@ -1,39 +1,18 @@
-import { useEffect, useState } from "react";
-import { fetchScores } from "../utils/leaderboard";
 import Score from "./Score";
 
-import { useParams } from "react-router-dom";
-
-const LeaderBoard = () => {
-	const { width, height, mines, lives } = useParams();
-	const [scores, setScores] = useState([]);
-
-	useEffect(() => {
-		fetchScores().then((scs) => {
-			let scores = scs;
-			if (width && height && mines && lives) {
-				scores = scores.filter((score) =>  (
-						score.size === width * height &&
-						score.mines === Number(mines) &&
-						score.lives === Number(lives)
-					));
-			}
-
-			return setScores(scores);
-		});
-	}, []);
-
+const LeaderBoard = ({ scoreList }) => {
 	return (
-		<div onClick={() => fetchScores().then(setScores)}>
-			<h1>Leaderboard</h1>
+		<>
 			<ol>
-				{scores.map((score) => (
-					<li key={score.id}>
-						<Score score={score} />
-					</li>
-				))}
+				{scoreList &&
+					scoreList.map &&
+					scoreList.map((score) => (
+						<li key={score.id}>
+							<Score score={score} />
+						</li>
+					))}
 			</ol>
-		</div>
+		</>
 	);
 };
 
