01dee0fa75e61abdcf76048224efc13b5f668947
Nico Diaz <nicodiazan@gmail.com>
2026-04-19 09:22:23 +0200

    Move the game board over to css grid

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -27,40 +27,38 @@ const Board = ({ board, onLeftClick, onRightClick, onHover = () => {} }) => {
 	// console.log(onHover)
 
 	return (
-		<div className="board">
-			{board.map((row, i) => (
-				<div key={i} className="row">
-					{row.map((cell, j) => (
-						<div
-							role="button"
-							tabIndex={0}
-							key={j}
-							className={
-								"cell" +
-								(cell.isRevealed ? " revealed" : "") +
-								(cell.isFlagged ? " flagged" : "") +
-								(typeof cell.content === "number"
-									? " " + getNumberName(cell.content)
-									: "")
-							}
-							onClick={(e) => onLeftClick(e, i, j)}
-							onContextMenu={(e) => onRightClick(e, i, j)}
-							onMouseEnter={(e) => onHover(e, [i, j])}
-							onMouseLeave={(e) => onHover(e, null)}
-						>
-							{!board[i][j].isRevealed
-								? board[i][j].isFlagged
-									? "🚩"
-									: " "
-								: board[i][j].isMine
-									? "💣"
-									: board[i][j].content == "0"
-										? " "
-										: board[i][j].content}
-						</div>
-					))}
-				</div>
-			))}
+		<div className="board" style={{ gridTemplateColumns: `repeat(${board.length}, 1fr)` }}>
+			{board.map((row, i) => {
+				return row.map((cell, j) => (
+					<div
+						role="button"
+						tabIndex={0}
+						key={j}
+						className={
+							"cell" +
+							(cell.isRevealed ? " revealed" : "") +
+							(cell.isFlagged ? " flagged" : "") +
+							(typeof cell.content === "number"
+								? " " + getNumberName(cell.content)
+								: "")
+						}
+						onClick={(e) => onLeftClick(e, i, j)}
+						onContextMenu={(e) => onRightClick(e, i, j)}
+						onMouseEnter={(e) => onHover(e, [i, j])}
+						onMouseLeave={(e) => onHover(e, null)}
+					>
+						{!board[i][j].isRevealed
+							? board[i][j].isFlagged
+								? "🚩"
+								: " "
+							: board[i][j].isMine
+								? "💣"
+								: board[i][j].content == "0"
+									? " "
+									: board[i][j].content}
+					</div>
+				));
+			})}
 		</div>
 	);
 };
